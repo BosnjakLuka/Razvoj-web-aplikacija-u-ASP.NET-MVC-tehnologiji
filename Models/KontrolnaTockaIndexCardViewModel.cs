935897b8b4c9d863a99644030824275583686179
lbosnjak0 <luka.bosnjak92@gmail.com>
2026-04-15 17:39:39 +0200

    Stranica KontrolnaTocka/Index
    funkcionalno i radi sve

---
+++ Models/KontrolnaTockaIndexCardViewModel.cs
@@ -0,0 +1,12 @@
+namespace planinarenje.Models;
+
+public class KontrolnaTockaIndexCardViewModel
+{
+    public int IdKontrolnaTocka { get; init; }
+    public string Naziv { get; init; } = string.Empty;
+    public string TipKontrolneTockeNaziv { get; init; } = string.Empty;
+    public int? NadmorskaVisina { get; init; }
+    public string PodrucjeNaziv { get; init; } = string.Empty;
+    public string? OpisPreview { get; init; }
+    public string? GUIDOznaka { get; init; }
+}
